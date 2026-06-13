2249f8370728b505dd55be7915f75c622103d5f6
Lenaa585 <justahuman917@gmail.com>
2026-06-13 18:33:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,27 +10,32 @@ app.get('/api/buscar', (req, res) => {
     const origen = req.query.origen;
     const destino = req.query.destino;
 
-    res.json([
-        {
-            compania: "Iberia",
-            origen: origen,
-            destino: destino,
-            precio: "145€"
-        },
-        {
-            compania: "Ryanair",
-            origen: origen,
-            destino: destino,
-            precio: "89€"
-        },
-        {
-            compania: "Vueling",
-            origen: origen,
-            destino: destino,
-            precio: "112€"
-        }
-    ]);
-
+  res.json([
+    {
+        compania: "Iberia",
+        origen: origen,
+        destino: destino,
+        salida: "08:30",
+        llegada: "10:15",
+        precio: "145€"
+    },
+    {
+        compania: "Ryanair",
+        origen: origen,
+        destino: destino,
+        salida: "12:45",
+        llegada: "14:30",
+        precio: "89€"
+    },
+    {
+        compania: "Vueling",
+        origen: origen,
+        destino: destino,
+        salida: "18:10",
+        llegada: "19:55",
+        precio: "112€"
+    }
+]);
 });
 
 // Configuración para Render
